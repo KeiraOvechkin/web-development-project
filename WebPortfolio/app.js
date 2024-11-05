e579a82923f72c431f47ee6789cf2c499ffeffbe
KeiraOvechkin <keiraxps2@gmail.com>
2024-11-05 01:36:43 +0000

    changes

--- WebPortfolio/app.js
+++ WebPortfolio/app.js
@@ -1,10 +1,10 @@
-const contactForm = document.querySelector('.contact-form');
+const contact = document.querySelector('.contact-form');
 
 let name = document.getElementById('name');
 let email = document.getElementById('email');
 let message = document.getElementById('message');
 
-contactForm.addEventListener('submit', (e)=>{
+contact.addEventListener('submit', (e)=>{
     e.preventDefault()
 
     let formData = {
@@ -16,5 +16,18 @@ contactForm.addEventListener('submit', (e)=>{
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/');
     xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            alert('Email sent');
+            name.value = ' ';
+            email.value = ' ';
+            message.value = ' ';
+        }else{
+            alert('Something went wrong!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
     
 })
--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const app = express();
 
+const nodemailer = require("nodemailer");
+
 const PORT = process.env.PORT || 5000;
 
 
 app.use(express.static('WebPortfolio'));
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/WebPortfolio/Contact.html')
 })
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'keiraxps2@gmail.com',
+            pass: 'Riley2004'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'keiraxps2@gmail.com',
+        message: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+
 
 app.listen(PORT, ()=>{
     console.log('Sever running on port ${PORT}')
